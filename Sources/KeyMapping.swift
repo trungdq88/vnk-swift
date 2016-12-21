0a7ea86d5727f026399b154e52142c22951998f7
Trung Dinh Quang <trungdq88@gmail.com>
2016-12-21 23:57:28 +0700

    replace single char

--- Sources/KeyMapping.swift
+++ Sources/KeyMapping.swift
@@ -3,7 +3,10 @@ import Cocoa
 class KeyMapping : NSObject {
     let BACKSPACE_SPECIAL_CHAR: String = "←";
     let ACCEPTED_CHARS = Array("1234567890[]',.pyfgcrl/=\\aoeuidhtns-;qjkxbmwvzPYFGCRLAOEUIDHTNSQJKXBMWVZ ".utf16)
+    let BUFFER_TERMINATE_CHARS = Array(" ".utf16)
     let BUFFERABLE_CHARS = Array("pyfgcrlaoeuidhtnsqjkxbmwvzPYFGCRLAOEUIDHTNSQJKXBMWVZ".utf16)
+    let MAX_BUFFER_LENGTH = 7;
+
     var kBuffer:Array<UniChar> = []
     func map(type: CGEventType, event: CGEvent) -> Array<UniChar> {
         switch type {
@@ -17,7 +20,6 @@ class KeyMapping : NSObject {
             return []
         }
     }
-
     func getEventChar(event: CGEvent) -> UniChar {
         let maxStringLength:Int = 1;
         let actualStringLength = UnsafeMutablePointer<Int>.allocate(capacity: 1);
@@ -39,12 +41,17 @@ class KeyMapping : NSObject {
             return []
         }
 
-        // Process here
-
-        if kBuffer.count > 0 && kBuffer[0] == Array("a".utf16)[0] && char == Array("a".utf16)[0] {
-            print("Change letter")
+        // Terminate buffer
+        if BUFFER_TERMINATE_CHARS.contains(char) {
             kBuffer.removeAll()
-            return Array("←â".utf16)
+            return []
+        }
+
+        // Process here
+        let modifierChars = transformBuffer(char: char);
+        print(modifierChars);
+        if modifierChars.count > 0 {
+            return modifierChars; // Ex: ← â
         }
 
         // Clear all buffer if current char is not bufferable
@@ -54,11 +61,39 @@ class KeyMapping : NSObject {
             return []
         }
 
+        if kBuffer.count > MAX_BUFFER_LENGTH {
+            print("Clear buffer because more than 1 char")
+            kBuffer.removeAll()
+        }
+
         print("Append to buffer")
         kBuffer.append(char)
         print(kBuffer)
 
         // print(kBuffer)
+        return []
+    }
+
+    func transformBuffer(char: UniChar) -> Array<UniChar> {
+        if let controlMap = inputMethod.getControlMap(char: char) {
+            print(controlMap);
+            var backspaceChars: Array<UniChar> = []
+            var repeatChars: Array<UniChar> = []
+            var isModified = false
+            for index in kBuffer.indices {
+                let currentChar = kBuffer[index];
+                if let posibleMappedChar = controlMap[currentChar] {
+                    kBuffer[index] = posibleMappedChar;
+                    isModified = true
+                }
+                if isModified {
+                    repeatChars.append(kBuffer[index])
+                    backspaceChars.append(Array(BACKSPACE_SPECIAL_CHAR.utf16)[0])
+                }
+            }
+            return backspaceChars + repeatChars
+        }
+        print("map not found for char \(char)");
         return []
     }
 
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,2 +1,9 @@
+import Cocoa
+
+func g(_ c: String) -> UniChar {
+    return Array(c.utf16)[0];
+}
+
 var keyMapping = KeyMapping()
+var inputMethod = InputMethod()
 _ = KeyEvent()
