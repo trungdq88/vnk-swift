29182de1bedb6d00d21b8673b7ea70d92da53e27
Trung Dinh Quang <trungdq88@gmail.com>
2016-12-31 18:32:41 +0700

    Test

--- Sources/Test.swift
+++ Sources/Test.swift
@@ -2,24 +2,39 @@ import Cocoa
 
 class KeyMappingTests : NSObject {
 
-    var tests : [String: [String: [UniChar]]] = [
-        "should do nothing with a": [
-            "input": Array("a".utf16),
-            "output": [],
-        ],
-        "should do nothing with b": [
-            "input": Array("b".utf16),
-            "output": [],
-        ],
-        "a8 -> ă": [
-            "input": Array("a8".utf16),
-            "output": Array("←ă".utf16),
-        ],
-        "tie61ng -> tiếng": [
-            "input": Array("tie61ng".utf16),
-            "output": Array("".utf16),
-        ],
-    ]
+    var tests : [String: [String: [[UniChar]]]] = [:]
+
+    override init() {
+        super.init()
+        tests["should do nothing with a"] = [
+        "input": [Array("a".utf16)],
+        "output": [[]],
+        ];
+
+        tests["should do nothing with b"] = [
+        "input": [Array("b".utf16)],
+        "output": [[]],
+        ];
+
+        tests["a8 -> ă"] = [
+        "input": [Array("a8".utf16)],
+        "output": [
+            Array("".utf16),
+            Array("←ă".utf16)
+        ]];
+
+        tests["tie61ng -> tiếng"] = [
+        "input": [Array("tie61ng".utf16)],
+        "output": [
+            Array("".utf16),
+            Array("".utf16),
+            Array("".utf16),
+            Array("←ê".utf16),
+            Array("←ế".utf16),
+            Array("".utf16),
+            Array("".utf16),
+        ]];
+    }
 
     func run() -> Bool {
         var testsCount = 0
@@ -27,19 +42,25 @@ class KeyMappingTests : NSObject {
         for (_, test) in tests.enumerated() {
             let key = KeyMapping()
             key.verbose = false
-            let inputChars = test.value["input"]
+            let inputChars = test.value["input"]![0]
             let expectedOutput = test.value["output"]
-            var output: [UniChar]!
+            var output: [[UniChar]] = []
             testsCount += 1;
 
-            for (_, char) in inputChars!.enumerated() {
-                output = key.receiveChar(char)
+            for (_, char) in inputChars.enumerated() {
+                output.append(key.receiveChar(char))
             }
 
             var result = ""
 
-            if output! == expectedOutput! {
+            if output.count == expectedOutput!.count {
                 result = "✅  PASS"
+                for (index, item) in expectedOutput!.enumerated() {
+                    if output[index] != item {
+                        result = "❌  FAIL"
+                        failCount += 1
+                    }
+                }
             } else {
                 result = "❌  FAIL"
                 failCount += 1
